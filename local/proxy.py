f82c67aa9f44c866b8cedb9610b9e44a5d1b6e7d
wwqgtxx <wwqgtxx@gmail.com>
2013-11-17 15:20:51 +0800

    add small tools to Generate rsa key
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- local/proxy.py
+++ local/proxy.py
@@ -1573,6 +1573,19 @@ def read_random_bits(nbits):
         randomdata = byte(randomvalue) + randomdata
     return randomdata
     
+def generate_RSA(bits=2048):
+    '''
+    Generate an RSA keypair with an exponent of 65537 in PEM format
+    param: bits The key length in bits
+    Return private key and public key
+    '''
+    from Crypto.PublicKey import RSA
+    new_key = RSA.generate(bits, e=65537)
+    public_key = new_key.publickey().exportKey("PEM")
+    private_key = new_key.exportKey("PEM")
+    print private_key
+    print public_key
+    
 def gae_urlfetch(method, url, headers, payload, fetchserver, **kwargs):
     # deflate = lambda x:zlib.compress(x)[2:-4]
     if payload:
